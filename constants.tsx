7733d7be048e567abf6e48b1587f63fe05b8d13c
Rodrigo Ferreira <61603333+rodrigoferreirasj@users.noreply.github.com>
2026-01-15 12:06:03 -0300

    Corrigido link do app de valores

--- constants.tsx
+++ constants.tsx
@@ -132,7 +132,7 @@ export const SECTIONS: Section[] = [
         title: "Ferramenta de Valores",
         description: "Descubra o que guia suas decisões",
         image: "https://cdn-icons-png.flaticon.com/512/1162/1162499.png",
-        url: "https://pontosfortes-values.netlify.app",
+        url: "https://pontosfortes-valores.netlify.app",
         type: 'tool'
       },
       {
